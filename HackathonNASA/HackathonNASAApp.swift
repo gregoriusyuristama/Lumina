25f56d8b157cdbb8514b92973ccef319e7493217
Gregorius Yuristama <102383943+gregoriusyuristama@users.noreply.github.com>
2023-10-07 13:41:28 +0700

    Initial Commit

---
+++ HackathonNASA/HackathonNASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonNASAApp.swift
+//  HackathonNASA
+//
+//  Created by Gregorius Yuristama Nugraha on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonNASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
